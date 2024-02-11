066a297ce07041592bcdadf914cdaf93abf28c52
Satyendra <satya@satyendra.in>
2024-02-11 11:48:36 +0530

    Update dependencies and assets

---
+++ lib/auth/gauth.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GAuth {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    if (googleUser != null) {
+      if (googleUser.email.endsWith('@iiitkota.ac.in')) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        final OAuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        return await _auth.signInWithCredential(credential);
+      } else {
+        // Delete the user
+        await _auth.currentUser?.delete();
+        await GAuth().signOut();
+        throw FirebaseAuthException(
+          code: 'ERROR_INVALID_EMAIL_DOMAIN',
+          message: 'Only @iiitkota.ac.in emails are allowed',
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    await _googleSignIn.signOut();
+  }
+}
